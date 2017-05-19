8971cc2db82748a6290dcbcd2967091d0c2a531a
James Klitzke <james.klitzke@gmail.com>
2017-05-18 19:33:28 -0500

    #2 code clean up.

--- ChatApp/ChatApp/OraChatAPI.swift
+++ ChatApp/ChatApp/OraChatAPI.swift
@@ -72,6 +72,8 @@ class OraChatAPI {
         }
     }
     
+    func postChat(name : String, message : String, )
+    
     func getChatHistory(success: @escaping ChatHistorySuccess, failure : @escaping ApiFailure) {
         let url = baseURL + chatsOp + "?page=1&limit=50"
         
